ff871dd1ff5857381dce89a61102a78dd2f48baf
Mohammad Al Amin <alamin.cse.99@gmail.com>
2024-07-13 06:28:57 +0600

    setup git

--- index.js
+++ index.js
@@ -349,3 +349,19 @@
         // console.log(insertionSort([2,23,4,89,-79]))
 
 
+// leet code problem solving
+
+// var twoSum = function(nums, target) {
+//     for(let i =0; i<nums.length; i++){
+//         for(let j = i+1; j<nums.length; j++){
+//                 if(nums[i] +nums [j]===target){
+//                         return [i, j]
+//                 }
+//         }
+//     }
+//     return false;
+// };
+
+// console.log(twoSum([3,2,4],6));
+
+console.log("hello");
